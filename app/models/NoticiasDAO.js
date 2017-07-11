0d0e58fd3f324cce05256eb865c29bd81ed2456c
Fabiana Oliveira <fabiana.rr.oliveira@gmail.com>
2017-07-11 15:21:15 -0300

    refactoring na logica nas pags noticias e noticia

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -5,11 +5,11 @@ function NoticiasDAO(connection){
 
 //propriedades que recebem uma função
 NoticiasDAO.prototype.getNoticias = function(callback){
-	this._connection.query('select * from noticias', callback); 
+	this._connection.query('select * from noticias order by data_criacao desc', callback); 
 }
 
 NoticiasDAO.prototype.getNoticia = function(callback){
-	this._connection.query('select * from noticias where id_noticia = 2', callback);
+	this._connection.query('select * from noticias where id_noticia = 10', callback);
 }
 
 NoticiasDAO.prototype.salvarNoticia = function(noticia, callback){
